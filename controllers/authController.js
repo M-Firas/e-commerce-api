fd8f1af05deef8790ec67b88f92a08bd8354ad42
Firas M <firas@testing.com>
2024-12-01 21:38:52 +0300

    Added Confirm Password feature in server-side

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,68 +1,74 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
-const {cookiesToResponse,createTokenUser} = require('../utils')
+const { cookiesToResponse, createTokenUser } = require('../utils')
 
 // user register controller
 const register = async (req, res) => {
-
- // check if the email is taken
-  const { email,name,lastName,password } = req.body;
+  // checking if the email is taken
+  const { email, name, lastName, password, confirmPassword } = req.body;
   const emailExists = await User.findOne({ email });
   if (emailExists) {
     throw new CustomError.BadRequestError("email is taken!");
   }
+  // registering first user as an admin
+  const isFirstAccount = await User.countDocuments({}) === 0;
+  const role = isFirstAccount ? 'admin' : 'user';
 
- // register first user as an admin
- const isFirstAccount = await User.countDocuments({}) === 0;
- const role = isFirstAccount? 'admin':'user'
+  // Creating a new user instance but not saving yet
+  const user = new User({ email, name, lastName, password, role });
 
- // creating a user and assinging JWT Token
- const user = await User.create({ email,name,lastName,password,role });
- const tokenUser = createTokenUser(user);
- 
+  // Setting the confirmPassword virtual field
+  user.confirmPassword = confirmPassword;
 
- //cookie setup
- cookiesToResponse({res,user:tokenUser})
+  // Saving the user (triggering pre('save') validation)
+  await user.save();
 
- res.status(StatusCodes.CREATED).json({ user: tokenUser });
+  //creating the user token
+  const tokenUser = createTokenUser(user);
+
+  // cookie setup
+  cookiesToResponse({ res, user: tokenUser });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
+
 
 // user login controller
 const login = async (req, res) => {
-  const {email,password} = req.body
+  const { email, password } = req.body
 
   //checking if email and password passed by the user
-  if(!email || !password){
+  if (!email || !password) {
     throw new CustomError.BadRequestError("please provide email and password");
   }
-  
+
   //checking if the user exists
   const user = await User.findOne({ email });
-  if(!user){
+  if (!user) {
     throw new CustomError.UnauthenticatedError('Invailed Email or Password')
   }
 
   //checking if the password is correct 
   const isPasswordCorrect = await user.comparePassword(password)
-  if(!isPasswordCorrect){
+  if (!isPasswordCorrect) {
     throw new CustomError.UnauthenticatedError('Invaild Email or Password')
   }
 
   //signing in the user
- const tokenUser = createTokenUser(user);
- cookiesToResponse({res,user:tokenUser})
- 
- res.status(StatusCodes.OK).json({ user: tokenUser });
+  const tokenUser = createTokenUser(user);
+  cookiesToResponse({ res, user: tokenUser })
+
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 
 // user logout controller
 const logout = async (req, res) => {
- res.cookie('token','logout', {
-  httpOnly:true,
-  expires: new Date(Date.now() + 5000),
- });
- res.status(StatusCodes.OK).json({msg:'user logged out'})
+  res.cookie('token', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now() + 5000),
+  });
+  res.status(StatusCodes.OK).json({ msg: 'user logged out' })
 };
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -1,52 +1,68 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcryptjs')
-
+const bcrypt = require('bcryptjs');
+const CustomError = require("../errors");
 
 const UserSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,'please provide a name'],
+    name: {
+        type: String,
+        required: [true, 'please provide a name'],
         minlength: 3,
         maxlength: 30,
     },
-    lastName:{
-        type:String,
-        required:[true,'please provide a last name'],
+    lastName: {
+        type: String,
+        required: [true, 'please provide a last name'],
         minlength: 2,
         maxlength: 30,
     },
-    email:{
-        type:String,
-        unique:true,
-        required:[true,'please provide an email'],
-        validate:{
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'please provide an email'],
+        validate: {
             validator: validator.isEmail,
-            message:'please provide a valid email'
-        }
+            message: 'please provide a valid email',
+        },
     },
-    password:{
-        type:String,
-        required:[true,'please provide a password'],
+    password: {
+        type: String,
+        required: [true, 'please provide a password'],
     },
-    role:{
-        type:String,
-        enum:['admin','user'],
-        default:'user',
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user',
+    },
+});
+
+// Hashing the password
+UserSchema.pre('save', async function (next) {
+    // If the password field is not modified, skip hashing
+    if (!this.isModified('password')) return next();
+    // Check if the passwords match (this._confirmPassword comes from virtual field)
+    if (this.password !== this._confirmPassword) {
+        throw new CustomError.BadRequestError('Passwords do not match');
     }
-})
 
-// hashing the password 
-UserSchema.pre('save',async function () {
-if(!this.isModified('password')) return;
-const salt = await bcrypt.genSalt(10);
-this.password = await bcrypt.hash(this.password,salt)
-})
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
-// comparing the passwords
-UserSchema.methods.comparePassword = async function(userPassword) {
-const isMatch = await bcrypt.compare(userPassword, this.password);
-return isMatch;
-}
+// Comparing the passwords
+UserSchema.methods.comparePassword = async function (userPassword) {
+    const isMatch = await bcrypt.compare(userPassword, this.password);
+    return isMatch;
+};
 
-module.exports = mongoose.model('User',UserSchema);
+// Virtual field for confirmPassword
+UserSchema.virtual('confirmPassword')
+    .set(function (value) {
+        this._confirmPassword = value;
+    })
+    .get(function () {
+        return this._confirmPassword;
+    });
+
+module.exports = mongoose.model('User', UserSchema);
