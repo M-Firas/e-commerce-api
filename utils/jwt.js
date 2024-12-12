6de950922e2ca80869d2592836e67d899265645c
Firas M <firas@testing.com>
2024-12-13 01:53:43 +0300

    modified JWT Cookie

--- utils/jwt.js
+++ utils/jwt.js
@@ -2,25 +2,26 @@ const jwt = require('jsonwebtoken');
 
 
 
-const createJWT = ({payload}) => {
-    const token = jwt.sign(payload,process.env.JWT_SECRET,{
-        expiresIn:process.env.JWT_LIFETIME
+const createJWT = ({ payload }) => {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME
     })
     return token;
 }
 
-const isTokenValid = ({token}) => jwt.verify(token,process.env.JWT_SECRET)
+const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET)
 
-const cookiesToResponse = ({res,user}) => {
-    const token = createJWT({payload:user})
+const cookiesToResponse = ({ res, user }) => {
+    const token = createJWT({ payload: user })
 
     const oneDay = 1000 * 60 * 60 * 24;
 
-    res.cookie('token',token, {
-     httpOnly:true,
-     expires: new Date(Date.now() + oneDay),
-     secure: process.env.NODE_ENV === 'production',
-     signed: true,
+    res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + oneDay),
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'none',
+        signed: true,
     })
 }
 
