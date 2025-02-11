4acb2c654941aa023877cd258d7d5f8049841f52
Firas M <firas@testing.com>
2025-02-12 02:28:28 +0300

    modified jwt

--- utils/jwt.js
+++ utils/jwt.js
@@ -17,6 +17,7 @@ const cookiesToResponse = ({ res, user, refreshToken }) => {
     res.cookie('accessToken', accessTokenJWT, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
+        sameSite: 'none',
         signed: true,
         expires: new Date(Date.now() + oneDay),
     })
@@ -24,10 +25,25 @@ const cookiesToResponse = ({ res, user, refreshToken }) => {
     res.cookie('refreshToken', refreshTokenJWT, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
+        sameSite: 'none',
         signed: true,
         expires: new Date(Date.now() + oneWeek),
     })
 }
+
+// const singleCookieToResponse = ({ res, user }) => {
+//     const token = createJWT({ payload: user })
+
+//     const oneDay = 1000 * 60 * 60 * 24;
+
+//     res.cookie('token', token, {
+//         httpOnly: true,
+//         expires: new Date(Date.now() + oneDay),
+//         secure: process.env.NODE_ENV === 'production',
+//         sameSite: 'none',
+//         signed: true,
+//     })
+// }
 
 
 module.exports = {
